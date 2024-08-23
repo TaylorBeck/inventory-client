3b9de7d0f332d54390ba5f55ebbe8d927eddd47a
Taylor Beck <beck.taylorg@gmail.com>
2024-08-23 04:41:34 -0700

    Setup dashboard layout and breakpoints

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,17 @@
+"use client"
+
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 dashboard-grid-rows h-[100vh]">
+      <div className="row-span-3 xl:row-span-6 bg-gray-500" />
+      <div className="row-span-3 xl:row-span-6 bg-gray-500" />
+      <div className="row-span-2 xl:row-span-3 col-span-1 md:col-span-2 xl:col-span-1 bg-gray-600" />
+      <div className="row-span-3 bg-gray-600" />
+      <div className="md:row-span-1 xl:row-span-2 bg-gray-400" />
+      <div className="md:row-span-1 xl:row-span-2 bg-gray-400" />
+      <div className="md:row-span-1 xl:row-span-2 bg-gray-400" />
+    </div>
   )
 }
 
-export default Dashboard
+export default Dashboard;
