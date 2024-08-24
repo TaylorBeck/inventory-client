bd5fe929500b9c5627dfaa0a5ff4abe453bce25d
Taylor Beck <beck.taylorg@gmail.com>
2024-08-23 19:39:19 -0700

    Add sales summary chart

--- src/app/dashboard/CardSalesSummary.tsx
+++ src/app/dashboard/CardSalesSummary.tsx
@@ -11,11 +11,13 @@ import {
   YAxis,
 } from "recharts";
 
+import { TrendingUp } from "lucide-react";
+
 const CardSalesSummary = () => {
   const { data, isLoading, isError } = useGetDashboardMetricsQuery();
   const salesData = data?.salesSummary || [];
   
-  const [timeframe, setTimeFrame] = useState("weekly");
+  const [timeFrame, setTimeFrame] = useState("weekly");
 
   const totalValueSum =
     salesData.reduce((accumulator, current) => {
@@ -47,7 +49,104 @@ const CardSalesSummary = () => {
 
   return (
     <div className="flex flex-col justify-between row-span-3 xl:row-span-6 bg-white shadow-md rounded-2xl pb-16">
-      
+      {isLoading ? (
+        <div className="m-5">Loading...</div>
+      ) : (
+        <>
+          <div>
+            <h2 className="text-lg font-semibold mb-2 px-7 pt-5">
+              Sales Summary
+            </h2>
+          </div>
+
+          <div>
+            <div className="flex justify-between items-center mb-6 px-7 mt-5">
+              <div className="text-lg font-medium">
+                <p className="text-xs text-gray-400">
+                  Value
+                </p>
+                <span className="text-2xl font-extrabold">
+                  $
+                  {(totalValueSum / 1000000).toLocaleString('en', {
+                    maximumFractionDigits: 2,
+                  })}
+                  m
+                </span>
+                <span className="text-green-500 text-sm ml-2">
+                  <TrendingUp className="inline 2-4 h-4 mr-1" />
+                  {averageChangePercentage.toFixed(2)}%
+                </span>
+              </div>
+              <select
+                className="shadow-sm border border-gray-300 bg-white p-2 rounded"
+                value={timeFrame}
+                onChange={(event) => {
+                  setTimeFrame(event.target.value) 
+                }}
+              >
+                <option value="daily">Daily</option>
+                <option value="weekly">Weekly</option>
+                <option value="monthly">Monthly</option>
+              </select>
+            </div>
+
+            {/* CHART */}
+            <ResponsiveContainer width="100%" height={350} className="px-7">
+              <BarChart
+                data={salesData}
+                margin={{ top: 0, right: 0, left: -25, bottom: 0 }}
+              >
+                <CartesianGrid strokeDasharray="" vertical={false} />
+                <XAxis
+                  dataKey="date"
+                  tickFormatter={(value) => {
+                    const date = new Date(value);
+                    return `${date.getMonth() + 1}/${date.getDate()}`;
+                  }}
+                />
+                <YAxis
+                  tickFormatter={(value) => {
+                    return `$${(value / 1000000).toFixed(0)}m`;
+                  }}
+                  tick={{ fontSize: 12, dx: -1 }}
+                  tickLine={false}
+                  axisLine={false}
+                />
+                <Tooltip
+                  formatter={(value: number) => [
+                    `$${value.toLocaleString('en')}`,
+                  ]}
+                  labelFormatter={(label) => {
+                    const date = new Date(label);
+                    return date.toLocaleDateString('en-US', {
+                      year: 'numeric',
+                      month: 'long',
+                      day: 'numeric',
+                    });
+                  }}
+                />
+                <Bar
+                  dataKey="totalValue"
+                  fill="#3182CE"
+                  barSize={10}
+                  radius={[10, 10, 0, 0]}
+                />
+              </BarChart>
+            </ResponsiveContainer>
+          </div>
+
+          <div>
+            <hr />
+            <div className="flex justify-between items-center mt-6 text-sm px-7 mb-4">
+              <p>{salesData.length || 0} days</p>
+              <p className="text-sm">
+                Highest Sales Date: {' '}
+                <span className="font-bold">{highestValueDate}</span>
+              </p>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
