7aedb33a147b4744d0daf1acfab8b19cd2e58274
Taylor Beck <beck.taylorg@gmail.com>
2024-08-24 19:54:17 -0700

    Fix navbar

--- src/app/(components)/Navbar/index.tsx
+++ src/app/(components)/Navbar/index.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 import Link from 'next/link';
 
-import { Bell, Menu, Moon, Settings, Sun } from 'lucide-react';
+import { Bell, Menu, Moon, Search, Settings, Sun } from 'lucide-react';
 import { setIsDarkMode, setIsSidebarCollapsed } from '@/state';
 import { useAppDispatch, useAppSelector } from '@/app/redux';
 
@@ -38,11 +38,11 @@ const Navbar = () => {
           <input
             type="search"
             placeholder="Search groups & products"
-            className="pl-10 pr-4 py-2 w-50 md:w-60 border-2 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500"
+            className="pl-10 pr-4 py-2 w-50 md:w-[220px] border-2 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500"
           />
 
           <div className="flex items-center absolute inset-y-0 left-0 pl-3 pointer-events-none">
-            <Bell
+            <Search
               className="text-gray-500"
               size={20}
             />
